47b153171a4ff343a30bc84158acfd14e9ce305f
Sergio de Diego <sergio.dediego@outlook.com>
2020-01-03 21:10:44 +0100

    Create block exception class

---
+++ backend/src/exceptions.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+
+class BaseError(Exception):
+    """
+    Handle generic exception.
+    """
+
+    def __init__(self, message: str = None, code: int = None):
+        """
+        Initialize BaseError class instance.
+
+        :param str message: error description message.
+        :param int code: error code number.
+        :return BaseError: new class instance.
+        """
+        super(BaseError, self).__init__(message, code)
+        if code is not None:
+            self.code = code
+            message = f'Error {self.code}. {message}'
+        self.message = message
+
+
+class BlockError(BaseError):
+    """
+    Handle exception for Block instances.
+    """
+    pass
