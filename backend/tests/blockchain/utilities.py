7dc25a17297fc9f6a4431110cffe449bc94833e2
Sergio de Diego <sergio.dediego@outlook.com>
2020-01-16 23:57:44 +0100

    Add test utilities for blockchain schema and model tests

--- backend/tests/blockchain/utilities.py
+++ backend/tests/blockchain/utilities.py
@@ -17,3 +17,20 @@ class BlockMixin(TestCase):
         mock_is_valid_schema.return_value = True
         testing_data = [random.randint(0, 100) for _ in range(10)]
         return Block.mine_block(block, testing_data)
+
+
+class BlockchainMixin(BlockMixin):
+
+    def _generate_valid_chain(self, length: int):
+        chain = [self._get_genesis_block()]
+        while len(chain) < length:
+            last_block = chain[-1]
+            chain.append(self._generate_block(last_block))
+        return chain
+
+    def _generate_invalid_chain(self):
+        chain = self.valid_chain[:]
+        block = self._generate_block(random.choice(chain[:-2]))
+        block.index = chain[-1].index + 1
+        chain.append(block)
+        return chain
