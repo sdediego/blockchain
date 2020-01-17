41c7521049c52e658e0168c64112c544c707e032
Sergio de Diego <sergio.dediego@outlook.com>
2020-01-17 19:50:16 +0100

    Add get blockchain API route

--- backend/src/app/api.py
+++ backend/src/app/api.py
@@ -6,6 +6,9 @@ from logging import getLogger
 from logging.config import fileConfig
 from os.path import dirname, join
 
+from src.blockchain.models.block import Block
+from src.blockchain.models.blockchain import Blockchain
+
 # Custom logger for api module
 fileConfig(join(dirname(dirname(__file__)), 'config', 'logging.cfg'))
 logger = getLogger(__name__)
@@ -13,6 +16,16 @@ logger = getLogger(__name__)
 
 app = FastAPI(__name__)
 
-@app.get("/")
+chain = [Block.genesis()]
+app.blockchain = Blockchain(chain)
+
+@app.get('/')
 async def root():
+    logger.info('[API] GET root.')
     return "Blockchain app is running"
+
+@app.get('/blockchain')
+async def blockchain():
+    logger.info(f'[API] GET blockchain.')
+    response = app.blockchain.serialize()
+    return {'blockchain': response}
