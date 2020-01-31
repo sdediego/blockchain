3b813775b5946798b7de16a8aff3f1e330d9a227
Sergio de Diego <sergio.dediego@outlook.com>
2020-01-31 21:22:52 +0100

    Add tests for client utils

---
+++ backend/tests/unit/client/test_utils.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+from unittest.mock import Mock, patch
+
+from src.client.models import utils
+from src.exceptions import WalletError
+from tests.unit.logging import LoggingMixin
+
+
+class ClientUtilsTest(LoggingMixin):
+
+    def setUp(self):
+        self.data = {'test_data_key': 'test_data_value'}
+
+    def test_utils_serialize(self):
+        serialized = utils.serialize(self.data)
+        self.assertIsInstance(self.data, dict)
+        self.assertIsInstance(serialized, str)
+
+    @patch('src.client.models.utils.json.dumps')
+    def test_utils_serialize_error(self, mock_json_dumps):
+        err_message = 'Could not encode data'
+        mock_json_dumps.side_effect = Mock(side_effect=WalletError(err_message))
+        with self.assertRaises(WalletError) as err:
+            utils.serialize(self.data)
+            self.assertTrue(mock_json_dumps.called)
+            self.assertIsInstance(err, WalletError)
+            self.assertIn(err_message, err.message)
