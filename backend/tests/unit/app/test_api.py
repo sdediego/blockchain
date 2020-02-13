22bfde213ba2e968570bc389a5bcffc23abed275
Sergio de Diego <sergio.dediego@outlook.com>
2020-02-13 22:26:31 +0100

    Update tests for transact API route

--- backend/tests/unit/app/test_api.py
+++ backend/tests/unit/app/test_api.py
@@ -29,21 +29,25 @@ class ApiTest(LoggingMixin):
         chain = response.json().get('blockchain').get('chain')
         self.assertEqual(len(chain), app.blockchain.length)
 
-    @patch('src.app.api.app.p2p_server.broadcast')
-    def test_api_get_mine_block_route(self, mock_broadcast):
-        mock_broadcast.return_value = asyncio.Future()
-        mock_broadcast.return_value.set_result(None)
+    @patch('src.app.api.app.p2p_server.broadcast_chain')
+    def test_api_get_mine_block_route(self, mock_broadcast_chain):
+        mock_broadcast_chain.return_value = asyncio.Future()
+        mock_broadcast_chain.return_value.set_result(None)
         response = self.client.get("/mine")
-        self.assertTrue(mock_broadcast.called)
+        self.assertTrue(mock_broadcast_chain.called)
         self.assertEqual(response.status_code, 200)
-        self.assertIn('new_block', response.json())
-        block_info = response.json().get('new_block')
+        self.assertIn('block', response.json())
+        block_info = response.json().get('block')
         self.assertEqual(Block.create(**block_info), app.blockchain.last_block)
 
-    def test_api_post_transact_route(self):
+    @patch('src.app.api.app.p2p_server.broadcast_transaction')
+    def test_api_post_transact_route(self, mock_broadcast_transaction):
+        mock_broadcast_transaction.return_value = asyncio.Future()
+        mock_broadcast_transaction.return_value.set_result(None)
         recipient = "recipient"
         amount = random.uniform(0, 100)
         response = self.client.post("/transact", json={"recipient": recipient, "amount": amount})
+        self.assertTrue(mock_broadcast_transaction.called)
         self.assertEqual(response.status_code, 200)
         self.assertIn('transaction', response.json())
         transaction = response.json().get('transaction')
