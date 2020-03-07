0892cdd3d8943bf4d36efc816b9506123d7eb4df
Sergio de Diego <sergio.dediego@outlook.com>
2020-03-07 21:42:02 +0100

    Add frontend mine transactions pool into block

--- frontend/src/components/TransactionsPool.js
+++ frontend/src/components/TransactionsPool.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import axios from 'axios';
 import Transaction from './Transaction';
 import { API_URL, TRANSACTIONS_INTERVAL } from '../config';
+import history from '../history';
 
 function TransactionsPool() {
   const [transactions, setTransactions] = useState([]);
@@ -13,6 +15,16 @@ function TransactionsPool() {
       .then(data => {
         const transactions = data.transactions;
         setTransactions(transactions);
+      });
+  };
+
+  const mineBlock = () => {
+    axios.get(`${ API_URL }/mine`)
+      .then(response => response.data)
+      .then(data => {
+        const block = data.block;
+        alert(`Success! Block mined: ${ JSON.stringify(block.hash) }`);
+        history.push('/blockchain');
       });
   };
 
@@ -32,6 +44,8 @@ function TransactionsPool() {
           return <div key={transaction.id}><hr /><Transaction transaction={transaction} /></div>;
         })}
       </div>
+      <hr />
+      <Button variant="primary" onClick={mineBlock}>Mine block</Button>
     </div>
   );
 }
